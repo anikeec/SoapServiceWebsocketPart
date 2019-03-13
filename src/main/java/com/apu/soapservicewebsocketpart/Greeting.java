32fa11f7b9cfb3f4341a47bbd845c8d0803d5ef6
apu <pasha_anik@ukr.net>
2019-03-13 16:32:36 +0200

    Init state

---
+++ src/main/java/com/apu/soapservicewebsocketpart/Greeting.java
@@ -0,0 +1,18 @@
+package com.apu.soapservicewebsocketpart;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ src/main/java/com/apu/soapservicewebsocketpart/MainController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apu.soapservicewebsocketpart;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+/**
+ *
+ * @author apu
+ */
+@Controller
+public class MainController {
+    
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+    
+}
