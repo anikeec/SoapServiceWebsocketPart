ce00801dd576fabefb8b07745bc2a05e1944194b
apu <pasha_anik@ukr.net>
2019-03-18 19:03:26 +0200

    Always redirect to one page after success login

--- src/main/java/com/apu/soapservicewebsocketpart/security/config/WebSecurityConfiguration.java
+++ src/main/java/com/apu/soapservicewebsocketpart/security/config/WebSecurityConfiguration.java
@@ -33,8 +33,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
         .formLogin()
         .loginPage("/login")
-        .failureUrl("/login?error=true")// login?error=true"       
-        .defaultSuccessUrl("/")
+        .failureUrl("/login?error=true")// login?error=true"
+        .defaultSuccessUrl("/", true)
         .usernameParameter("email")
         .passwordParameter("password")
         .and().logout()
